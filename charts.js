9b7081fac913c021807b4e13dc515975a00d50ed
munno88 <munno88@hotmail.com>
2022-08-21 16:58:06 -0700

    change color

--- charts.js
+++ charts.js
@@ -124,7 +124,7 @@ function buildCharts(sample) {
         marker: {
           size: sample_values,
           color: otu_ids,
-          colorscale: "Earth"
+          colorscale: "Portland"
         }
       }
     ];
